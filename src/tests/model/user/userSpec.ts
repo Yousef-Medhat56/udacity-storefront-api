53dfe1f3788aaae92807fb82d3d6570cff10a7a3
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-06 12:31:40 +0200

    test (index) method - user model

--- src/tests/model/user/userSpec.ts
+++ src/tests/model/user/userSpec.ts
@@ -6,10 +6,13 @@ import { dummyUser } from "./dummydata";
 describe("Test User Model", () => {
     const store = new UserStore();
 
-    it("Test create method", async () => {
+    it("Test (create) method", async () => {
         const data = await store.create(dummyUser);
-        expect(data).toEqual(
-            jasmine.objectContaining({ first_name: dummyUser.firstName })
-        );
+        expect(data).toEqual(jasmine.objectContaining(dummyUser));
+    });
+
+    it("Test (index) method", async () => {
+        const data = await store.index();
+        expect(data).toContain({ id: 1, ...dummyUser });
     });
 });
