7059534c280c92633f3d7453ac0bfa8c25f1c7b2
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-06 14:46:07 +0200

    add (show) method - product model

--- src/model/product.ts
+++ src/model/product.ts
@@ -41,4 +41,18 @@ export default class ProductStore{
             throw new Error(`Could not get products. Error: ${error}`);
         }
     }
+
+    //show specific user
+    async show(id:number):Promise<Product>{
+        try {
+            const connection = await (client as Pool).connect();
+            const sql = "SELECT * FROM products WHERE id=$1";
+            const result = await connection.query(sql,[id]);
+            connection.release();
+
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Could not get product. Error: ${error}`);
+        }
+    }
 }
