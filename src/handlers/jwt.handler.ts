1427853b1f1e9774302da54feb6bde2d2bb27ac9
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-07 14:05:23 +0200

    configure dotenv library

--- src/handlers/jwt.handler.ts
+++ src/handlers/jwt.handler.ts
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
-class JwtHandler {
+export default class JwtHandler {
     private secret: string | undefined = process.env.TOKEN_SECRET;
-    
+
     //sign new token
-    sign(id: number):string {
+    sign(id: number): string {
         const token = jwt.sign({ id }, this.secret as string);
-        return token
+        return token;
     }
 }
