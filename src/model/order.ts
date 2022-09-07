515ae14c82b154817d330ee7eec5074355c88661
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-07 10:07:36 +0200

    add (createProductOrder) method - order model

--- src/model/order.ts
+++ src/model/order.ts
@@ -8,9 +8,17 @@ export type Order = {
     status: "active" | "completed";
 };
 
+/* create type ProductOrder
+ represents the record inserted in the join table between Products and Orders*/
+export type ProductOrder = {
+    order_id: number;
+    product_id: number;
+    quantity: number;
+};
+
 export default class OrderStore {
     //create new order
-    async create(newOrder: Order) {
+    async create(newOrder: Order): Promise<Order> {
         try {
             const connection = await (client as Pool).connect();
             const sql =
@@ -21,5 +29,23 @@ export default class OrderStore {
         } catch (error) {
             throw new Error(`Could not create order. Error: ${error}`);
         }
+    }
+
+    // create new record in the join table between products and orders
+    async createProductOrder(
+        newProductOrder: ProductOrder
+    ): Promise<ProductOrder> {
+        const connection = await (client as Pool).connect();
+        const sql =
+            "INSERT INTO products_orders(order_id,product_id,quantity) VALUES($1,$2,$3) RETURNING *";
+
+        // destruct newProductOrder object
+        const { order_id, product_id, quantity } = newProductOrder;
+        const result = await connection.query(sql, [
+            order_id,
+            product_id,
+            quantity,
+        ]);
+        return result.rows[0];
     }
 }
