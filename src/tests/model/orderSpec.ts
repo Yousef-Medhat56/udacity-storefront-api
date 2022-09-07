7b8730ca3490d51c1e3cad51c47a75e4682f97d5
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-07 09:42:22 +0200

    test (create) method - order model

---
+++ src/tests/model/orderSpec.ts
@@ -0,0 +1,20 @@
+import OrderStore, { Order } from "../../model/order";
+
+const dummyOrder: Order = {
+    user_id: 1,
+    status: "active",
+};
+
+export default function testOrderModel() {
+    describe("Test Order model", () => {
+        const store = new OrderStore();
+
+        it("Test (create) method", async () => {
+            const data = await store.create(dummyOrder);
+            
+            expect(data).toEqual(
+                jasmine.objectContaining(dummyOrder)
+            );
+        });
+    });
+}
