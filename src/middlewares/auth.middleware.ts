b1841ad53234a2039122a2864c8c6e668840924b
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-09 09:46:35 +0200

    add (verify token) middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+//check if the user is authorized to go to the next endpoint or not
+
+import { Request, Response, NextFunction } from "express";
+import JwtHandler from "../handlers/jwt.handler";
+
+//JWT handler
+const jwt = new JwtHandler();
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        //the authorization header
+        const { authorization } = req.headers;
+        //jwt token
+        const token = (authorization as string).split(" ")[1];
+        // decode the token
+        const userDecoded = jwt.decode(token) as { id: number };
+
+        //if the user is authorised
+        if (userDecoded.id) {
+            res.locals.user_id = userDecoded.id;
+            next();
+        }
+        //if the user is not authorised
+        else throw new Error();
+    } catch (error) {
+        res.status(401).json({ error: "Unauthorized" });
+    }
+};
+
+export { verifyToken };
