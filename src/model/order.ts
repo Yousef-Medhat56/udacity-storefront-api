8c103830b873e41bb6698bb48e46623b4248fdec
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-10 09:28:34 +0200

    create (updateProductOrder) method that updates a record in the join table between products and orders

--- src/model/order.ts
+++ src/model/order.ts
@@ -31,6 +31,19 @@ export default class OrderStore {
         }
     }
 
+    //update order
+    async update(order_id: number): Promise<Order> {
+        try {
+            const connection = await (client as Pool).connect();
+            const sql =
+                "UPDATE orders SET status = 'completed' WHERE id = $1 RETURNING *";
+            const result = await connection.query(sql, [order_id]);
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Could not update order. Error: ${error}`);
+        }
+    }
+
     // create new record in the join table between products and orders
     async createProductOrder(
         newProductOrder: ProductOrder
@@ -53,16 +66,29 @@ export default class OrderStore {
         }
     }
 
-    //update order
-    async update(id: number): Promise<Order> {
+    // update a record in the join table between products and orders
+    async updateProductOrder(
+        newProductOrder: ProductOrder
+    ): Promise<ProductOrder> {
         try {
             const connection = await (client as Pool).connect();
             const sql =
-                "UPDATE orders SET status = 'completed' WHERE id = $1 RETURNING *";
-            const result = await connection.query(sql, [id]);
-            return result.rows[0];
+                "UPDATE products_orders SET quantity = $3 WHERE order_id=$1 AND product_id=$2 RETURNING *";
+
+            // destruct newProductOrder object
+            const { order_id, product_id, quantity } = newProductOrder;
+            const result = await connection.query(sql, [
+                order_id,
+                product_id,
+                quantity,
+            ]);
+            //if there are results, return it
+            if (result.rows.length) {
+                return result.rows[0];
+            }
+            throw new Error();
         } catch (error) {
-            throw new Error(`Could not update order. Error: ${error}`);
+            throw new Error(`Could not update ProductOrder. Error: ${error}`);
         }
     }
 }
