2ab838ae27d79e0379d4a62efdbb5346ba8e01a2
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-10 09:58:46 +0200

    add PUT /orders

---
+++ src/routes/orders.route.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+// import middlewares - validators
+import validate from "../middlewares/validate.middleware";
+import orderValidationRules from "../middlewares/validators/orders.validator";
+
+//import middlewares - authentication
+import verifyToken from "../middlewares/auth.middleware";
+
+//import controllers
+import OrdersController from "../controllers/orders.controller";
+
+const router = Router();
+
+//orders controller
+const controller = new OrdersController();
+
+//add a record in the products_orders table
+router.put(
+    "/",
+    verifyToken,
+    orderValidationRules(),
+    validate,
+    controller.addProductToOrder
+);
+
+export default router;
