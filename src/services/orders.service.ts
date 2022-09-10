25da7250a8db787405b63cab945f5efa02f5e54a
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-09-10 09:39:28 +0200

    create (getProductIdsBtOrderId) that returns array of product ids in the order

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -12,23 +12,19 @@ class OrderServices extends OrderStore {
         return result.rows[0].id;
     }
 
-    async getProductsInOrder(order_id: number) {
-        const connection = await (client as Pool).connect();
-        const sql = `SELECT product_id, name AS product_name,price AS product_price,quantity FROM products_orders 
-            JOIN products ON products.id=products_orders.product_id AND products_orders.order_id = $1`;
-
-        const result = await connection.query(sql, [order_id]);
-        connection.release();
-        return result.rows;
-    }
-
+    //return array of product ids in the order
     async getProductIdsBtOrderId(order_id: number) {
         const connection = await (client as Pool).connect();
         const sql = "SELECT product_id FROM products_orders WHERE order_id=$1";
         const result = await connection.query(sql, [order_id]);
         connection.release();
+
+        //product ids array
         const productIds: number[] = [];
+
+        //itterate through each row in the results
         for (const row of result.rows) {
+            //push the product id to the product ids array
             productIds.push(row.product_id);
         }
 
